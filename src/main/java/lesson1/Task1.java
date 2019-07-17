295ae59a77697a8eb3eb6a6ccbec743c1a246ead
Antony <antony@mail.com>
2019-07-17 16:16:07 +0300

    1-5 tasks done

--- src/main/java/lesson1/Task1.java
+++ src/main/java/lesson1/Task1.java
@@ -5,10 +5,15 @@ public class Task1 {
     Написать функцию, которая вычисляет минимум из трёх чисел.
     */
     public static void main(String[] args) {
-        //System.out.println(min(5,2,3));
+        System.out.println(min(5, 2, 3));
     }
 
     public static int min(int a, int b, int c) {
-        return 0;
+        if (b >= a && c >= a) {
+            return a;
+        } else if (a >= b && c >= b) {
+            return b;
+        } else return c;
     }
+
 }
--- src/main/java/lesson1/Task5.java
+++ src/main/java/lesson1/Task5.java
@@ -6,10 +6,16 @@ package lesson1;
  */
 public class Task5 {
     public static void main(String[] args) {
-        //System.out.println(reverse("Hello world!!!"));
+        System.out.println(reverse("Hello world!!!"));
     }
 
     public static String reverse(String str) {
-        return "";
+        char[] revArray = str.toCharArray();
+        String resultStr = "";
+        for (int i = revArray.length-1; i>=0; i--){
+            //System.out.print(revArray[i]);
+            resultStr += revArray[i];
+        }
+        return resultStr;
     }
 }
